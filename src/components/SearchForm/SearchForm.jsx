95aef27ed5c6472f16e7a7ae3b324a05d288ec3f
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-11-25 21:14:14 +0100

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { nanoid } from 'nanoid';
+
+import { useDispatch } from 'react-redux';
+import { addTodoAction } from 'redux/todoSlice';
+
+import { useLocalStorage } from 'hooks/useLocalStorage';
 
 import { FiSearch } from 'react-icons/fi';
+
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export const SearchForm = ({ onSubmit }) => {
-  const [query, setQuery] = useState('');
+export const SearchForm = () => {
+  const dispatch = useDispatch();
 
-  const handleInput = ({ currentTarget }) => setQuery(currentTarget.value);
+  const [value, setValue] = useLocalStorage('search', '');
+
+  const handleInput = ({ currentTarget }) => setValue(currentTarget.value);
 
   const handleSubmit = e => {
     e.preventDefault();
-    onSubmit(query);
-    setQuery('');
+
+    const todo = {
+      id: nanoid(),
+      name: value,
+    };
+
+    dispatch(addTodoAction(todo));
+    setValue('');
   };
 
   return (
@@ -24,7 +39,7 @@ export const SearchForm = ({ onSubmit }) => {
         placeholder="What do you want to write?"
         name="search"
         required
-        value={query}
+        value={value}
         autoFocus
       />
     </SearchFormStyled>
