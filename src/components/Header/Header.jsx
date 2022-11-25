419a74ce1013dad6701a168ead0bea475445a699
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-11-25 21:51:16 +0100

    refactoring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,22 @@
-import { IoLogoReact } from 'react-icons/io5';
-import { NavBar, HeaderWrapper, NavText } from './Header.styled';
-import { Container } from 'components';
 import { useTheme } from '@emotion/react';
+import { IoLogoReact } from 'react-icons/io5';
+
+import { Container } from 'components';
+
+import { NavBar, HeaderWrapper, NavText } from './Header.styled';
 
 export const Header = () => {
   const theme = useTheme();
 
   return (
-    <>
-      <NavBar>
-        <Container>
-          <HeaderWrapper>
-            <IoLogoReact size="40px" color={theme.colors.accent} />
+    <NavBar>
+      <Container>
+        <HeaderWrapper>
+          <IoLogoReact size="40px" color={theme.colors.accent} />
 
-            <NavText>Lesson 4</NavText>
-          </HeaderWrapper>
-        </Container>
-      </NavBar>
-    </>
+          <NavText>Lesson 4</NavText>
+        </HeaderWrapper>
+      </Container>
+    </NavBar>
   );
 };
